61811e77c93980737b39040a2ea2f2ae40b9c2c0
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-04 23:33:31 +0200

    username & email view added

--- expenses_app/authentication/views.py
+++ expenses_app/authentication/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+from validate_email import validate_email
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error: email already exists'}, status=409)
+        return JsonResponse({'email_valid': True})
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error':'username should only contain alphanumeric values'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error: username already exists'}, status=409)
+        return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
