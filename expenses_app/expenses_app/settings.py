01e6060294054fce6db10c380b9d5f0b5e15e0be
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-04 13:52:39 +0200

    connect db

--- expenses_app/expenses_app/settings.py
+++ expenses_app/expenses_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'expenses_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'income_expensesdb',
+        'USER': 'postgres',
+        'PASSWORD': 'tgkcapture1212',
+        'HOST': 'localhost',
+
     }
 }
 
