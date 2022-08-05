79cc2a75918897b53c73ecfd76bdf02621ffae50
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-05 18:10:22 +0200

    views addition

--- expenses_app/authentication/views.py
+++ expenses_app/authentication/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 import json
 from validate_email import validate_email
+from django.contrib import messages
+
 
 class EmailValidationView(View):
     def post(self, request):
@@ -16,20 +18,48 @@ class EmailValidationView(View):
             return JsonResponse({'email_error: email already exists'}, status=409)
         return JsonResponse({'email_valid': True})
 
+
 class UsernameValidationView(View):
     def post(self, request):
         data = json.loads(request.body)
         username = data['username']
 
         if not str(username).isalnum():
-            return JsonResponse({'username_error':'username should only contain alphanumeric values'}, status=400)
+            return JsonResponse({'username_error': 'username should only contain alphanumeric values'}, status=400)
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error: username already exists'}, status=409)
         return JsonResponse({'username_valid': True})
 
+
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+    def post(self, request):
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+
+                if len(password) < 6:
+                    messages.error(
+                        request, 'Password should be at least 6 characters long')
+                    return render(request, 'authentication/register.html', context)
+
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.set_password(password)
+                user.save()
+                messages.success(request, 'Registration successful')
+                return render(request, 'authentication/register.html')
+
 
 class LoginView(View):
     def get(self, request):
