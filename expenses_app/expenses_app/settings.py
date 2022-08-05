1402189b90881476d8d21b4b3be1eef090762e84
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-06 00:39:10 +0200

    email settings

--- expenses_app/expenses_app/settings.py
+++ expenses_app/expenses_app/settings.py
@@ -138,6 +138,7 @@ MESSAGE_TAGS = {
 }
 
 # email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'           
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tawongakanyenda5@gmail.com'
